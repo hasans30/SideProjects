a49edf83dc593bf276aa076ef7ac2cdccfc5083c
Hasan <msheikh@microsoft.com>
2018-09-04 06:41:50 -0700

    initial code for react app

---
+++ src/components/EmployeesPage.js
@@ -0,0 +1,29 @@
+import React  from 'react';
+
+
+class EmployeesPage extends React.Component 
+{
+
+    render()
+    {
+        return (
+            <div>
+                <h1> hello </h1>
+                <table className="table">
+      <thead>
+      <tr>
+        <th>&nbsp;</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Phone</th>
+        <th>Dept.</th>
+      </tr>
+      </thead>
+      <tbody>
+          </tbody>
+          </table>
+            </div>
+        );
+    }
+}
+export default EmployeesPage;
