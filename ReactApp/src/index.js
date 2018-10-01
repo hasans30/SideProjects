b29e6afdaa756bcb89e039f46cb0ed0b74fb55e0
Hasan <msheikh@microsoft.com>
2018-09-30 22:09:27 -0700

    added reducer,redux and thunk. yet to code how UI should reflect when items loaded

--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store/configureStore'
 import registerServiceWorker from './registerServiceWorker';
+import {loadEmployee} from './actions/employeeActions';
+
+//create store
+const store = configureStore();
+debugger;
+store.dispatch(loadEmployee());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
