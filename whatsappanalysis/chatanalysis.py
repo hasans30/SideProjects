ca32bef50d767a37ef9f78cac71d8836e3705441
Hasan uz zaman Sheikh <hasan@live.in>
2020-01-08 16:23:35 -0800

    added IST aspect

--- whatsappanalysis/chatanalysis.py
+++ whatsappanalysis/chatanalysis.py
@@ -6,7 +6,7 @@ def parse_file(text_file):
     '''Convert WhatsApp chat log text file to a Pandas dataframe.'''
     
     # some regex to account for messages taking up multiple lines
-    pat = re.compile(r'^(\d\d\/\d\d\/\d\d\d\d.*?)(?=^^\d\d\/\d\d\/\d\d\d\d|\Z)', re.S | re.M)
+    pat = re.compile(r'^(\d{1,2}\/\d{1,2}\/\d{2}.*?)(?=^\d{1,2}\/\d{1,2}\/\d{2}.*?|\Z)', re.S | re.M)
     with open(text_file) as f:
         data = [m.group(1).strip().replace('\n', ' ') for m in pat.finditer(f.read())]
 
@@ -18,7 +18,7 @@ def parse_file(text_file):
 
         # sender is between am/pm, dash and colon
         try:
-            s = re.search('m - (.*?):', row).group(1)
+            s = re.search('[mM] - (.*?):', row).group(1)
             sender.append(s)
         except:
             sender.append('')
@@ -30,7 +30,7 @@ def parse_file(text_file):
             message.append('')
 
     df = pd.DataFrame(zip(datetime, sender, message), columns=['timestamp', 'sender', 'message'])
-    df['timestamp'] = pd.to_datetime(df.timestamp, format='%d/%m/%Y, %I:%M %p')
+    df['timestamp'] = pd.to_datetime(df.timestamp )+ pd.Timedelta('13:30:00')
 
     # remove events not associated with a sender
     df = df[df.sender != ''].reset_index(drop=True)
