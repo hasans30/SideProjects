6e678e2213eb181b86f64e2b239a22541370125a
Hasan uz zaman Sheikh <hasan@live.in>
2020-02-20 09:59:49 -0800

    Code to scrap web and send email notification

---
+++ WebSiteScrappingAndEmailing/getTable.py
@@ -0,0 +1,20 @@
+﻿import re
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+
+if ( len(sys.argv) != 2 ):
+    print('no filename argument passed')
+    exit(1)
+filename=sys.argv[1]
+
+vgm_url = 'https://www.immihelp.com/visa-bulletin-movement/eb1-india/filing'
+html_text = requests.get(vgm_url).text
+soup = BeautifulSoup(html_text, 'html.parser')
+
+table = soup.find('table', {'class':'graphTable'})
+strtable=str(table)
+with open(filename, encoding='utf-8', mode='w') as f:
+    f.write(strtable)
+        
---
+++ WebSiteScrappingAndEmailing/sendEmailImmihelp.ps1
@@ -0,0 +1,29 @@
+﻿function cleanup {
+
+Param ($fileName)
+if ( Test-Path $fileName ) {
+    Write-Output "Cleaning up file $($fileName)"
+    Remove-Item $fileName -Force
+} else {
+    Write-Output "$($fileName) doesnt exists";
+}
+
+}
+
+
+# get web page
+$htmlFile="_tmp.html";
+cleanup($htmlFile);
+$url="https://www.immihelp.com/visa-bulletin-movement/eb1-india/filing";
+python $pwd/getTable.py $htmlFile
+$body=$(gc $htmlFile|Out-String)
+#send email;
+$password = ConvertTo-SecureString $Env:EMAILHELP -AsPlainText -Force
+$smtpCred= New-Object System.Management.Automation.PSCredential ($Env:EMAILUSER, $password) ;
+Write-Host "credential $($smtpCred)"
+
+
+Send-MailMessage -From $Env:EMAILUSER -To $env:EMAILTO -Subject "immihelp visa bulletin" -Body $body -DeliveryNotificationOption OnFailure -SmtpServer 'smtp.office365.com' -Credential $smtpCred -Port 587 -UseSsl -BodyAsHtml
+
+
+
