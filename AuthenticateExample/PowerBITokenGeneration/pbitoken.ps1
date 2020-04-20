055b81395c6e6c39161c8a3763cb95aac333b506
Hasan <hasan@live.in>
2020-04-20 13:10:26 -0700

    all pbi and only pbi token in seperate file

--- AuthenticateExample/PowerBITokenGeneration/pbitoken.ps1
+++ AuthenticateExample/PowerBITokenGeneration/pbitoken.ps1
@@ -35,36 +35,40 @@ function Get-GraphAccessTokenFromMSAL {
 }
 
 
+function Get-PBIToken($bearer){
+$auth="Bearer $bearer"
 
+
+$pbitoken=Invoke-WebRequest -Uri "https://api.powerbi.com/v1.0/myorg/GenerateToken" -Method "POST" -Headers @{"method"="POST"; "authority"="api.powerbi.com"; "scheme"="https"; "path"="/v1.0/myorg/GenerateToken"; "pragma"="no-cache"; "cache-control"="no-cache"; "authorization"=$auth; "sec-fetch-dest"="empty"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"; "accept"="*/*"; "origin"="https://docs.microsoft.com"; "sec-fetch-site"="cross-site"; "sec-fetch-mode"="cors"; "referer"="https://docs.microsoft.com/en-us/rest/api/power-bi/embedtoken/generatetoken"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9,pt;q=0.8,ar;q=0.7"} -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes("{$([char]10)  `"datasets`": [$([char]10)    {$([char]10)      `"id`": `"2478bd4f-cc1b-4e3a-8566-922aa78865ab`"$([char]10)    }$([char]10)  ],$([char]10)  `"reports`": [$([char]10)    {$([char]10)      `"allowEdit`": true,$([char]10)      `"id`": `"6afe69fc-354b-40cc-a735-2eccb7dc5d6c`"$([char]10)    }  ]$([char]10)}$([char]10)"))
+
+ write-output $(" export const pbiToken = $pbitoken") | tee -FilePath C:\gitrepo\Aria\honolulu-client\src\features\power-bi-poc\config.js
+}
+
+# main 
 $file="_auth_"
 touch $file
 $atk=(gc $file)
- 
+
 if( $genauth -ne 0){
 $atk=Get-GraphAccessTokenFromMSAL
 $atk|out-file $file
+Get-PBIToken($atk)
+} else {
+
+try {
+Get-PBIToken($atk)
+
+
+} catch {
+
+$atk=Get-GraphAccessTokenFromMSAL
+$atk|out-file $file
+Get-PBIToken($atk)
+
+
 }
-$auth="Bearer $atk"
 
 
-$groups=Invoke-WebRequest -Uri "https://api.powerbi.com/v1.0/myorg/groups" -Headers @{"method"="GET"; "authority"="api.powerbi.com"; "scheme"="https"; "path"="/v1.0/myorg/groups"; "pragma"="no-cache"; "cache-control"="no-cache"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"; "sec-fetch-dest"="empty"; "authorization"=$auth; "accept"="*/*"; "origin"="https://docs.microsoft.com"; "sec-fetch-site"="cross-site"; "sec-fetch-mode"="cors"; "referer"="https://docs.microsoft.com/en-us/rest/api/power-bi/groups/getgroups"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9,pt;q=0.8,ar;q=0.7"}
 
-$groups.Content
-
-$datasets=Invoke-WebRequest -Uri "https://api.powerbi.com/v1.0/myorg/groups/e91aaac8-c690-4aba-b69a-fc6228778f14/datasets" -Headers @{"method"="GET"; "authority"="api.powerbi.com"; "scheme"="https"; "path"="/v1.0/myorg/groups/e91aaac8-c690-4aba-b69a-fc6228778f14/datasets"; "pragma"="no-cache"; "cache-control"="no-cache"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"; "sec-fetch-dest"="empty"; "authorization"=$auth; "accept"="*/*"; "origin"="https://docs.microsoft.com"; "sec-fetch-site"="cross-site"; "sec-fetch-mode"="cors"; "referer"="https://docs.microsoft.com/en-us/rest/api/power-bi/datasets/getdatasetsingroup"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9,pt;q=0.8,ar;q=0.7"}
-
-$datasets.Content
-
-
-$reports=Invoke-WebRequest -Uri "https://api.powerbi.com/v1.0/myorg/groups/e91aaac8-c690-4aba-b69a-fc6228778f14/reports" -Headers @{"method"="GET"; "authority"="api.powerbi.com"; "scheme"="https"; "path"="/v1.0/myorg/groups/7be1f5e2-10aa-4447-9cb3-b9768d5d6d9f/reports"; "pragma"="no-cache"; "cache-control"="no-cache"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"; "sec-fetch-dest"="empty"; "authorization"=$auth; "accept"="*/*"; "origin"="https://docs.microsoft.com"; "sec-fetch-site"="cross-site"; "sec-fetch-mode"="cors"; "referer"="https://docs.microsoft.com/en-us/rest/api/power-bi/reports/getreportsingroup"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9,pt;q=0.8,ar;q=0.7"}
-
-$reports.Content
-
-write-host "try to get embedded token"
-
-$pbitoken=Invoke-WebRequest -Uri "https://api.powerbi.com/v1.0/myorg/GenerateToken" -Method "POST" -Headers @{"method"="POST"; "authority"="api.powerbi.com"; "scheme"="https"; "path"="/v1.0/myorg/GenerateToken"; "pragma"="no-cache"; "cache-control"="no-cache"; "authorization"=$auth; "sec-fetch-dest"="empty"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"; "accept"="*/*"; "origin"="https://docs.microsoft.com"; "sec-fetch-site"="cross-site"; "sec-fetch-mode"="cors"; "referer"="https://docs.microsoft.com/en-us/rest/api/power-bi/embedtoken/generatetoken"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9,pt;q=0.8,ar;q=0.7"} -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes("{$([char]10)  `"datasets`": [$([char]10)    {$([char]10)      `"id`": `"aa206b8a-9e83-45a8-9a47-3deb3485fd2b`"$([char]10)    }$([char]10)  ],$([char]10)  `"reports`": [$([char]10)    {$([char]10)      `"allowEdit`": true,$([char]10)      `"id`": `"8d8931e1-4491-46fd-80ae-0b5f7c87a7a3`"$([char]10)    }  ]$([char]10)}$([char]10)"))
-
-$pbitoken.Content
-
-
+}
 
